4e57459bf8a64ff84a9da6de963bcafe55722991
Adeniji Charles Ayodipupo <adenijiayocharles@gmail.com>
2022-06-05 08:45:55 +0200

    add comment to auth tesst file

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -11,29 +11,33 @@ class AuthTest extends TestCase
 {
     use WithFaker;
 
+    /**
+     * registration test
+     *
+     * @return  void
+     */
     public function test_registration(): void
     {
         $password = $this->faker->password();
-
         $response = $this->postJson(
-            "api/register",
+            'api/register',
             [
-                "name" => $this->faker->firstName() . " " . $this->faker->lastName(),
-                "email" => $this->faker->email(),
-                "password" => $password,
-                "password_confirmation" => $password
+                'name' => $this->faker->firstName() . ' ' . $this->faker->lastName(),
+                'email' => $this->faker->email(),
+                'password' => $password,
+                'password_confirmation' => $password
             ],
             [
-                "Accept" => "application/json",
-                "Content-Type" => "application/json"
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/json'
             ]
         );
         $response->assertStatus(201)
             ->assertJsonStructure([
-                "status",
-                "message",
-                "data" => [
-                    "auth_token"
+                'status',
+                'message',
+                'data' => [
+                    'auth_token'
                 ]
             ]);
     }
@@ -49,22 +53,22 @@ class AuthTest extends TestCase
         $email = $this->faker->email();
 
         User::factory()->create([
-            "name" => $this->faker->firstName() . " " . $this->faker->lastName(),
-            "email" => $email,
-            "password" => $password
+            'name' => $this->faker->firstName() . ' ' . $this->faker->lastName(),
+            'email' => $email,
+            'password' => $password
         ]);
 
         $loginData = [
-            "email" => $email,
-            "password" => $password
+            'email' => $email,
+            'password' => $password
         ];
 
-        $this->postJson("api/login", $loginData, ["Accept" => "application/json", "Content-Type" => "application/json"])
+        $this->postJson('api/login', $loginData, ['Accept' => 'application/json', 'Content-Type' => 'application/json'])
             ->assertStatus(200)->assertJsonStructure([
-                "status",
-                "message",
-                "data" => [
-                    "auth_token"
+                'status',
+                'message',
+                'data' => [
+                    'auth_token'
                 ]
             ]);
     }
