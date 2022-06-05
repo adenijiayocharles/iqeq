9636e20b4c4366d6aea74a856b71a0afdfcc1348
Adeniji Charles Ayodipupo <adenijiayocharles@gmail.com>
2022-06-05 13:48:30 +0200

    add manufacturer factory and seeder

---
+++ database/factories/ManufacturerFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ManufacturerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $faker = (new \Faker\Factory())::create();
+        $faker->addProvider(new \Faker\Provider\Fakecar($faker));
+        return [
+            "name" => $faker->vehicleBrand
+        ];
+    }
+}
